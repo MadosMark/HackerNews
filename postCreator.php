bc03cb1faa3c8fa3615bf30a16d91ae7aee87099
MadosMark <69796358+MadosMark@users.noreply.github.com>
2021-01-11 17:16:24 +0100

    New Post Css - Done

--- postCreator.php
+++ postCreator.php
@@ -1,43 +1,24 @@
-<?php require __DIR__ . '/homepage/header.php';
+<?php require __DIR__ . '/homepage/header.php'; ?>
+<?php require __DIR__ . '/homepage/navbar.php'; ?>
 
-$user = confirmUser($pdo);
-?>
+<?php $user = confirmUser($pdo); ?>
 
-<?php if (isset($_SESSION['errors'])) {  ?>
-    <p class="error-message"> <?php errorMessage();
-                                unset($_SESSION['errors']); //delete error message after displayed
-                            } ?> </p>
+<?php if (!isset($_SESSION['user'])) : ?>
+    <p> You need to be logged in to submit posts! </p>
+<?php endif; ?>
 
+<?php if (isset($_SESSION['user'])) : ?>
+    <form action="/web/post/store.php" method="post">
+        <div class="create_post_container">
+            <label for="title"> Title </label>
+            <input type="text" name="title" id="title" placeholder="Enter title" required>
+            <label for="description"> Description </label>
+            <input type="text" name="description" id="description" placeholder="Enter text" required>
+            <label for="url"> Url to the post </label>
+            <input type="url" name="url" id="url" placeholder=" Enter url" required>
+            <button type="submit" class="create_post_button"> Create post </button>
+        </div>
+    </form>
+<?php endif; ?>
 
-
-    <?php
-    if (!isset($_SESSION['user'])) : ?>
-        <p> You need to be logged in to submit posts! </p>
-
-    <?php endif; ?>
-
-
-    <?php if (isset($_SESSION['user'])) : ?>
-
-        <section class="sign-up-form">
-            <form action="/web/post/store.php" method="post">
-                <div class="sign-up">
-                    <label for="title"> Title </label>
-                    <input type="text" name="title" id="title" required>
-
-                    <label for="description"> Description </label>
-                    <input type="text" name="description" id="description" placeholder="Paragraf" required>
-
-                    <label for="url"> Url to the post </label>
-                    <input type="url" name="url" id="url" placeholder="www.hackernews.com" required>
-
-
-                    <button type="submit" class="submit-button"> Create post </button>
-
-
-            </form>
-            </div>
-
-        </section>
-
-    <?php endif; ?>
+<?php require __DIR__ . '/homepage/footer.php'; ?>
